8587d9b10fab01b7ae489c6e2cf222751e59e8c6
Jamie Magee <jamie.magee@gmail.com>
2014-01-14 11:44:28 +0000

    Cache comments, exit gracefully, respond to "Slow Down" requests

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
-import praw, urllib2, re, cgi, ConfigParser, time, os
+import praw, urllib2, re, cgi, ConfigParser, time, os, pickle, atexit
 
 HTTPRE = re.compile('http[s]?:\/\/', re.IGNORECASE)
 DOMRE = re.compile('\.\w{2,20}')
-COMRE = re.compile('is (.*) down\?', re.IGNORECASE)
 
 FOOTER = '''
 
@@ -11,11 +10,41 @@ FOOTER = '''
 bot by [Jammie1](http://reddit.com/u/Jammie1)
 '''
 
+def exit_handler():
+  if fileOpened:
+    pickle.dump(already_done, f)
+    f.close()
+
+atexit.register(exit_handler)
+
 def valid_response_code(code):
   if (code == 200) or (code == 301) or (code == 302):
     return True
   else:
     return False
+    
+def isDone(comment):
+  if comment.id not in already_done and comment.author.name !="IsItDownBot":
+    comment_replies = comment.replies
+    for reply in comment_replies:
+      if reply.author.name == "IsItDownBot":
+        already_done.append(comment.id)
+        return True
+    return False
+  else:
+    return True
+    
+def reply(text, comment):
+  while True:
+    try:
+      comment.reply(text)
+      break
+    except praw.errors.RateLimitExceeded as error:
+      print ("Doing too much, sleeping for " + str(error.sleep_time))
+      time.sleep(error.sleep_time)
+    except Exception as e:
+      print ("Exception occurred while replying: " + str(e))
+      time.sleep(3)
 
 class Url:
   def __init__(self, domain):
@@ -46,6 +75,15 @@ class Url:
       return True
     else:
       return False
+      
+already_done = []
+if(os.path.isfile('commentcache')):
+  f = open('commentcache', 'r+')
+  if f.tell() != os.fstat(f.fileno()).st_size:
+    already_done = pickle.load(f)
+  f.close()
+f = open('commentcache', 'w+')
+fileOpened = True
 
 r = praw.Reddit('/u/IsItDownBot by /u/Jammie1')
 
@@ -57,38 +95,40 @@ if os.path.isfile('settings.cfg'):
 else:
   username = os.environ['REDDIT_USERNAME']
   password = os.environ['REDDIT_PASSWORD']
+  
+COMRE = re.compile('/u/' + username +' (.*)', re.IGNORECASE)
 
 print '[*] Logging in as %s...' % username
 r.login(username, password)
 print '[*] Login successful...\n'
 
-already_done = set()
 while True:
   subreddit = r.get_subreddit('jammie1')
   subreddit_comments = subreddit.get_comments()
-  print '[*] Getting submissions...\n'
+  print '[*] Getting comments...\n'
 
   for comment in subreddit_comments:
+    answered = isDone(comment)
     if COMRE.search(comment.body) and comment.id not in already_done:
       u = Url(COMRE.search(comment.body).group(1))
       if u.missingdomain():
         print("Huh? " + u.domain + " doesn't look like a site on the interwho.")
-        comment.reply("Huh? " + u.domain + " doesn't look like a site on the interwho." + FOOTER)
-        already_done.add(comment.id)
+        reply("Huh? " + u.domain + " doesn't look like a site on the interwho." + FOOTER, comment)
+        already_done.append(comment.id)
       else:
         try:
           response = urllib2.urlopen(u.domain).code
         except urllib2.URLError:
           print("Huh? " + u.domain + " doesn't look like a site on the interwho.")
-          comment.reply("Huh? " + u.domain + " doesn't look like a site on the interwho." + FOOTER)
-          already_done.add(comment.id)
+          reply("Huh? " + u.domain + " doesn't look like a site on the interwho." + FOOTER, comment)
+          already_done.append(comment.id)
         else:
           if valid_response_code(response):
             print("It's just you. " + u.domain + " is up.")
-            comment.reply("It's just you. " + u.domain + " is up." + FOOTER)
-            already_done.add(comment.id)
+            reply("It's just you. " + u.domain + " is up." + FOOTER, comment)
+            already_done.append(comment.id)
           else:
             print("It's not just you! " + u.domain + " looks down from here.")
-            comment.reply("It's not just you! " + u.domain + " looks down from here." + FOOTER)
-            already_done.add(comment.id)
+            reply("It's not just you! " + u.domain + " looks down from here." + FOOTER, comment)
+            already_done.append(comment.id)
   time.sleep(2)
